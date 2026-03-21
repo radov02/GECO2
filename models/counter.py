20015a3f250b422a8ad9dbbb3a2d86a0d4d63870
Erik Radovicevic <radovicevic.erik1@gmail.com>
2026-03-21 19:12:45 +0100

    torch hub set

--- models/counter.py
+++ models/counter.py
@@ -36,8 +36,6 @@ class CNT(nn.Module):
         self.image_size = image_size
         self.zero_shot = zero_shot
         self.pretrain = False
-        # TODO REMOVE!!
-        torch.hub.set_dir('/d/hpc/projects/FRI/pelhanj/CNT_SAM2/models/')
         self.class_embed = nn.Sequential(nn.Linear(emb_dim, 1), nn.LeakyReLU())
         self.bbox_embed = MLP(emb_dim, emb_dim, 4, 3)
         if not self.pretrain:
